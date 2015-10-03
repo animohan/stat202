7232bd21228cb3b3b2a8cbb609e158741e15e92b
animohan <anishmohan@gmail.com>
2015-10-03 13:50:53 -0700

    HW1 Updates

--- HW1/P6/Test1.R
+++ HW1/P6/Test1.R
@@ -1,5 +1,16 @@
+#Q6 Part a
+in_training_target<-read.csv("training_target.csv")
+print(paste0("Number of patients: ", nrow(in_training_target)))
+
+
+summary(in_training_target["ALSFRS_slope"])
+
 
 # Q6. Part b
+
+in_training_features<-read.csv("training_features.csv")
+print(paste0("Number of features: ", ncol(in_training_features)))
+
   numnas<-function(x){
     sum_na<-0
     for (n in x){
@@ -9,23 +20,16 @@
     return(sum_na)
   }
   
-  #in_training_features<-read.csv("training_features.csv")
-  in_training_features<-matrix(data=c("one",1,NA,3,"two",2,NA,4,"three",5,NA,7),nrow=4,ncol=3)
+
   num_nas_col=apply(in_training_features,2,numnas)
   
-  # Iterarive Code
-    # iter<-seq(1:ncol(B))
-    # k=length(iter)
-    # C = matrix(0,nrow=k, ncol=1)
-    # 
-    # for(count in iter){
-    #   C[count,1]=numnas(B[,count])
-    # }
+
   
   hist(num_nas_col)
 
 #Q6 Part c
   feature.names<-names(in_training_features)
+  
   for(feature.name in feature.names[-1]){
     dummy_name<-paste0("is.na.",feature.name)
     is.na.feature <-is.na(in_training_features[,feature.name])
@@ -33,3 +37,20 @@
     in_training_features[is.na.feature,feature.name]<-median(in_training_features[,feature.name], na.rm=TRUE)
     
   }
+  
+
+#Q6 Part d
+valid_features<-read.csv("validation_features.csv")
+valid_target<-read.csv("validation_target.csv")
+print(paste0("Number of validation patients: ", nrow(valid_target)))
+print(paste0("Number of validation features: ", ncol(valid_features)))
+
+
+summary(in_training_target["ALSFRS_slope"])
+summary(valid_target["ALSFRS_slope"])
+
+summary(valid_features["weight.slope"])
+
+
+
+  
