99a3e88d99d9ebb6560ca8d2b3027fa610b873bb
anishmohan@gmail.com <anishmohan@gmail.com>
2015-10-19 06:10:57 -0700

    HW4 4 questions

---
+++ HW4/HW4.R
@@ -0,0 +1,108 @@
+attach(Default)
+set.seed(1)
+glm.fit=glm(default~income+balance, family=binomial)
+
+#cross validation
+train=sample(10000,6000)
+train_default=(Default[train,])
+test_default=(Default[-train,])
+glm.fit=glm(default~income+balance, train_default,family=binomial)
+glm.probs=predict(glm.fit,test_default,type="response")
+glm.pred=rep("No",4000)
+glm.pred[glm.probs>0.5]="Yes"
+table(glm.pred,test_default$default)
+mean(glm.pred!=test_default$default)
+
+
+train=sample(10000,2000)
+train_default=(Default[train,])
+test_default=(Default[-train,])
+glm.fit=glm(default~income+balance, train_default,family=binomial)
+glm.probs=predict(glm.fit,test_default,type="response")
+glm.pred=rep("No",8000)
+glm.pred[glm.probs>0.5]="Yes"
+table(glm.pred,test_default$default)
+mean(glm.pred!=test_default$default)
+
+train=sample(10000,5000)
+train_default=(Default[train,])
+test_default=(Default[-train,])
+glm.fit=glm(default~income+balance, train_default,family=binomial)
+glm.probs=predict(glm.fit,test_default,type="response")
+glm.pred=rep("No",5000)
+glm.pred[glm.probs>0.5]="Yes"
+table(glm.pred,test_default$default)
+mean(glm.pred!=test_default$default)
+
+train=sample(10000,8000)
+train_default=(Default[train,])
+test_default=(Default[-train,])
+glm.fit=glm(default~income+balance, train_default,family=binomial)
+glm.probs=predict(glm.fit,test_default,type="response")
+glm.pred=rep("No",2000)
+glm.pred[glm.probs>0.5]="Yes"
+table(glm.pred,test_default$default)
+mean(glm.pred!=test_default$default)
+
+
+train=sample(10000,5000)
+train_default=(Default[train,])
+test_default=(Default[-train,])
+glm.fit=glm(default~income+balance+student, train_default,family=binomial)
+glm.probs=predict(glm.fit,test_default,type="response")
+glm.pred=rep("No",5000)
+glm.pred[glm.probs>0.5]="Yes"
+table(glm.pred,test_default$default)
+mean(glm.pred!=test_default$default)
+
+
+#Q2 (6 in book)
+set.seed(1)
+glm.fit=glm(default~income+balance,Default,family=binomial)
+summary(glm.fit)
+
+boot.fn =function(data, index){
+  train_data=data[index,]
+  glm.fit=glm(default~income+balance,train_data,family=binomial)
+  return(glm.fit$coefficients)
+}
+
+boot(data=Default, statistic = boot.fn, R=1000)
+
+#Q3 (8 in book)
+
+set.seed(1)
+y=rnorm(100)
+x=rnorm(100)
+y=x-2*x^2+rnorm(100)
+plot(x,y)
+
+set.seed(2)
+glm.fit=glm(y~x, data=xydat)
+cv.err=cv.glm(xydat,glm.fit)
+cv.err$delta[1]
+
+glm.fit=glm(y~poly(x,2), data=xydat)
+cv.err=cv.glm(xydat,glm.fit)
+cv.err$delta[1]
+
+glm.fit=glm(y~poly(x,3), data=xydat)
+cv.err=cv.glm(xydat,glm.fit)
+cv.err$delta[1]
+
+glm.fit=glm(y~poly(x,4), data=xydat)
+cv.err=cv.glm(xydat,glm.fit)
+cv.err$delta[1]
+
+
+#Q4 ( 9 in book)
+library(MASS)
+
+boot.fn2 = function(inputdata,index){
+  Boston_temp=inputdata[index,]
+  return (mean(Boston_temp$medv))
+}
+
+boot(data=Boston, statistic = boot.fn2, R=500)
+
+
