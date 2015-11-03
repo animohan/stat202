a327ac579eea4d6a9bb3d31487958bee1b8bb5ae
anishmohan@gmail.com <anishmohan@gmail.com>
2015-11-03 07:11:56 -0800

    HW5 prob 4

--- HW5/Lab1.R
+++ HW5/Lab1.R
@@ -23,4 +23,17 @@ plot(reg.summary$cp, xlab="Number of variables", ylab="Cp",type="l")
 which.min(reg.summary$cp)
 points(10,reg.summary$cp[10],col="red",cex=2,pch=20)
 
+#Test
+X=rnorm(100)
+eps=rnorm(100)
+X2=X^2
+X3=X^3
+Y=5+1*X+2*X2+4*X3+eps
+
+df=data.frame(Y)
+for(i in 1:10){
+ df[paste0('X',i)]=X^i
+}
+
+regfit.X=regsubsets(df$Y~., data=df,nvmax=10)
 
--- HW5/Lab2.R
+++ HW5/Lab2.R
@@ -17,3 +17,19 @@ ridge.mod$lambda[50]
 sqrt(sum(coef(ridge.mod)[-1,50]^2))
 sqrt(sum(coef(ridge.mod)[-1,60]^2))
 predict(ridge.mod, s=50, type="coefficients")[1:20]
+
+set.seed(1)
+train=sample(1:nrow(x),nrow(x)/2)
+test=(-train)
+y.test=y[test]
+ridge.mod=glmnet(x[train,],y[train], alpha=0, lambda=grid, thresh=1e-12)
+ridge.pred=predict(ridge.mod,s=4, newx=x[test,])
+mean((ridge.pred-y.test)^2)
+
+
+#Test
+X=rnorm(100)
+eps=rnorm(100)
+X2=X^2
+X3=X^3
+Y=5+1*X+2*X2+4*X3+eps
