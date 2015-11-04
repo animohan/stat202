80e6c7890d33c13098899ce5324bde7ff6cd9f18
animohan <anishmohan@gmail.com>
2015-11-03 17:53:15 -0800

    HW5 updates

--- HW5/Lab2.R
+++ HW5/Lab2.R
@@ -45,3 +45,34 @@ library(leaps)
 df=data.frame(y=Y,x=X)
 ridge.X=regsubsets(y~poly(x,10,raw=T), data=df, nvmax=10)
 ridge.summary=summary(ridge.X)
+
+
+
+
+
+library(ISLR)
+fix(Hitters)
+names(Hitters)
+dim(Hitters)
+sum(is.na(Hitters$Salary))
+Hitters=na.omit(Hitters)
+dim(Hitters)
+
+library(leaps)
+regfit.full=regsubsets(Salary~.,Hitters)
+summary(regfit.full)
+
+regfit.full=regsubsets(Salary~.,data=Hitters, nvmax=19)
+reg.summary=summary(regfit.full)
+
+par(mfrow=c(2,2))
+plot(reg.summary$rss, xlab="Number of variables", ylab="RSS",type = "l")
+plot(reg.summary$adjr2, xlab="Number of variables", ylab="Adjusted RSq",type = "l")
+which.max(reg.summary$adjr2)
+points(11,reg.summary$adjr2[11],col="red",cex=2,pch=20)
+
+plot(reg.summary$cp, xlab="Number of variables", ylab="Cp",type="l")
+which.min(reg.summary$cp)
+points(10,reg.summary$cp[10],col="red",cex=2,pch=20)
+
+#Test
