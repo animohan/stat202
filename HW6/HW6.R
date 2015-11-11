9c6372393fd0055ef63a8ef98c9152d1c6513f3e
anishmohan@gmail.com <anishmohan@gmail.com>
2015-11-11 07:28:32 -0800

    HW6 complete

--- HW6/HW6.R
+++ HW6/HW6.R
@@ -180,3 +180,106 @@ for(i in 1:5 ) {
   }
 }
 plot(x,y)
+
+
+
+x=rnorm(100)
+eps=rnorm(100)
+y=5+2*x+7*x*x+3*x*x*x+5*eps
+plot(x,y)
+
+u0=lm(y~1)
+u1=lm(y~poly(x,1))
+u2=lm(y~poly(x,2))
+u3=lm(y~poly(x,3))
+u4=lm(y~poly(x,4))
+u5=lm(y~poly(x,5))
+
+u0derv=rep(0,5)
+u0derv[1]=sum(u0$coefficients[1])
+u0derv[2]=sum(u0$coefficients[1])
+u0derv[3]=sum(u0$coefficients[1])
+u0derv[4]=sum(u0$coefficients[1])
+u0derv[5]=sum(u0$coefficients[1])
+
+u1derv=rep(0,5)
+u1derv[1]=sum(u1$coefficients[1]+u1$coefficients[2]*x)
+u1derv[2]=sum(u1$coefficients[2])
+u1derv[3]=sum(u1$coefficients[1]+u1$coefficients[2]*x)
+u1derv[4]=sum(u1$coefficients[1]+u1$coefficients[2]*x)
+u1derv[5]=sum(u1$coefficients[1]+u1$coefficients[2]*x)
+
+u2derv=rep(0,5)
+u2derv[1]=sum(u2$coefficients[1]+u2$coefficients[2]*x+u2$coefficients[3]*x*x)
+u2derv[2]=sum(u2$coefficients[2]+2*u2$coefficients[3]*x)
+u2derv[3]=sum(2*u2$coefficients[3])
+u2derv[4]=sum(u2$coefficients[1]+u2$coefficients[2]*x+u2$coefficients[3]*x*x)
+u2derv[5]=sum(u2$coefficients[1]+u2$coefficients[2]*x+u2$coefficients[3]*x*x)
+
+u3derv=rep(0,5)
+u3derv[1]=sum(u3$coefficients[1]+u3$coefficients[2]*x+u3$coefficients[3]*x*x+u3$coefficients[4]*x*x*x)
+u3derv[2]=sum(u3$coefficients[2]+2*u3$coefficients[3]*x+3*u3$coefficients[4]*x*x)
+u3derv[3]=sum(2*u3$coefficients[3]+6*u3$coefficients[4]*x)
+u3derv[4]=sum(6*u3$coefficients[4])
+u3derv[5]=sum(u3$coefficients[1]+u3$coefficients[2]*x+u3$coefficients[3]*x*x+u3$coefficients[4]*x*x*x)
+
+u4derv=rep(0,5)
+u4derv[1]=sum(u4$coefficients[1]+u4$coefficients[2]*x+u4$coefficients[3]*x*x+u4$coefficients[4]*x*x*x+u4$coefficients[5]*x*x*x*x)
+u4derv[2]=sum(u4$coefficients[2]+2*u4$coefficients[3]*x+3*u4$coefficients[4]*x*x+4*u4$coefficients[5]*x*x*x)
+u4derv[3]=sum(2*u4$coefficients[3]+6*u4$coefficients[4]*x+12*u4$coefficients[5]*x*x)
+u4derv[4]=sum(6*u4$coefficients[4]+24*u4$coefficients[5]*x)
+u4derv[5]=100*sum(24*u4$coefficients[5])
+
+plot(x,y)
+xlim=range(x)
+xgrid=seq(from=xlim[1],to=xlim[2], length.out = 100)
+pred=predict(u1,newdata=list(x=xgrid),se=T)
+lines(xgrid, pred$fit, lwd=2)
+
+plot(x,y)
+xlim=range(x)
+xgrid=seq(from=xlim[1],to=xlim[2], length.out = 100)
+pred=predict(u2,newdata=list(x=xgrid),se=T)
+lines(xgrid, pred$fit, lwd=2)
+
+plot(x,y)
+xlim=range(x)
+xgrid=seq(from=xlim[1],to=xlim[2], length.out = 100)
+pred=predict(u3,newdata=list(x=xgrid),se=T)
+lines(xgrid, pred$fit, lwd=2)
+
+
+plot(x,y)
+xlim=range(x)
+xgrid=seq(from=xlim[1],to=xlim[2], length.out = 100)
+pred=predict(u4,newdata=list(x=xgrid),se=T)
+lines(xgrid, pred$fit, lwd=2)
+
+#a) lambda=10^15, m=0
+lambda=10^15
+penalty=rep(NA,5)
+penalty[1]=sum(y-u1$coefficients[1])+lambda*((u0derv[1])^2)
+penalty[2]=sum(u1$residuals)+lambda*((u1derv[1])^2)
+penalty[3]=sum(u2$residuals)+lambda*((u2derv[1])^2)
+penalty[4]=sum(u3$residuals)+lambda*((u3derv[1])^2)
+penalty[5]=sum(u4$residuals)+lambda*((u4derv[1])^2)
+which.min(penalty)
+
+lambda=10^15
+penalty=rep(NA,5)
+penalty[1]=sum(y-u1$coefficients[1])+lambda*((u0derv[2])^2)
+penalty[2]=sum(u1$residuals)+lambda*((u1derv[2])^2)
+penalty[3]=sum(u2$residuals)+lambda*((u2derv[2])^2)
+penalty[4]=sum(u3$residuals)+lambda*((u3derv[2])^2)
+penalty[5]=sum(u4$residuals)+lambda*((u4derv[2])^2)
+which.min(penalty)
+
+lambda=10^15
+penalty=rep(NA,5)
+penalty[1]=sum(y-u1$coefficients[1])+lambda*((u0derv[3])^2)
+penalty[2]=sum(u1$residuals)+lambda*((u1derv[3])^2)
+penalty[3]=sum(u2$residuals)+lambda*((u2derv[3])^2)
+penalty[4]=sum(u3$residuals)+lambda*((u3derv[3])^2)
+penalty[5]=sum(u4$residuals)+lambda*((u4derv[3])^2)
+which.min(penalty)
+
